c1f1e686c85a93c8e806f04fd28bf02bf890e4c0
Biswa <barikbiswa143@gmail.com>
2023-05-24 14:31:02 +0530

    mailbox

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+
+const Home = () => {
+  return (
+    <>
+      <Typography variant="h4" color="initial">
+        welcome
+      </Typography>
+    </>
+  );
+};
+
+export default Home;
